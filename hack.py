75b67bae31128eba36066110ea77f62d1458da42
aNOOBis <69043738+aNOOBisTheGod@users.noreply.github.com>
2020-10-01 16:19:48 +0300

    E

--- hack.py
+++ hack.py
@@ -1,10 +1,10 @@
 import os
 import random
 
-for i in range(1):
+for i in range(20):
     with open('data.txt', 'a') as f:
         f.write(f'{i}')
-        x = 365 + random.randint(0, 1)
+        x = 365 + random.randint(0, i)
         date = f'{x} days ago'
         os.system('git add .')
         os.system(f'git commit --date="{date}" -m "E"')
